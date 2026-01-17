138aaa3b37a90cb6683c1bffc648c53c552e621b
efra8ab <efra8ab@gmail.com>
2026-01-17 01:01:02 -0600

    W01 Individual Activity: Add Item to Cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,8 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cart = getLocalStorage("so-cart") || [];
+  const cartItems = Array.isArray(cart) ? cart : [cart];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
